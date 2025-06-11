99b2248e9d5ced439c0eee0939a0caad3f28fc12
coderzstudio <coderz360@gmail.com>
2025-06-11 15:47:14 +0530

    Update Books.tsx

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -117,7 +117,7 @@ const Books: React.FC = () => {
               />
             </div>
             <Select value={selectedCategory} onValueChange={setSelectedCategory}>
-              <SelectTrigger className="w-15 h-8">
+              <SelectTrigger className="w-8 h-8">
                 <SelectValue placeholder="Category" />
               </SelectTrigger>
               <SelectContent>
